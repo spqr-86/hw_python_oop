dad954cf90e5988dde97aa29266cd5c0c644f4a3
Petr <spqr86.sr@gmail.com>
2020-10-09 03:16:34 +0800

    6

--- homework.py
+++ homework.py
@@ -6,38 +6,41 @@ class Calculator:
         self.limit = limit
         self.records = []
 
+    today = dt.date.today()
+
     def add_record(self, record):
 
         self.records.append(record)
 
     def get_today_stats(self):
-        today_stats = 0
-        for i in self.records:
-            if i.date == dt.datetime.now().date():
-                today_stats += i.amount
-        return today_stats
+        return sum(e.amount for e in self.records if e.date == self.today)
 
     def get_week_stats(self):
-        week_stats = 0
-        for i in self.records:
-            if (dt.datetime.now().date() + dt.timedelta(1)) \
-                    > i.date >= (dt.datetime.now().date() - dt.timedelta(7)):
-                week_stats += i.amount
-        return week_stats
+        return sum(e.amount for e in self.records if
+                   (self.today + dt.timedelta(1)) > e.date >=
+                   (self.today - dt.timedelta(7)))
 
 
 class Record:
-    def __init__(self, amount, comment, date=str(dt.datetime.now().strftime('%d.%m.%Y'))):
+
+    DATE_FORMAT = '%d.%m.%Y'
+
+    def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
-        date_format = '%d.%m.%Y'
-        moment = dt.datetime.strptime(date, date_format)
-        self.date = moment.date()
+        if date is None:
+            self.date = dt.date.today()
+        else:
+            moment = dt.datetime.strptime(date, self.DATE_FORMAT)
+            self.date = moment.date()
 
 
 class CaloriesCalculator(Calculator):
-    def __init__(self, limit):
-        super().__init__(limit)
+
+    LEFT = 'Сегодня можно съесть что-нибудь ещё, ' \
+           'но с общей калорийностью не более {} кКал'
+
+    STOP = 'Хватит есть!'
 
     def get_calories_remained(self):
 
@@ -46,42 +49,34 @@ class CaloriesCalculator(Calculator):
         calories_remained = self.limit - today_stats
 
         if calories_remained > 0:
-            return f'Сегодня можно съесть что-нибудь ещё, ' \
-                f'но с общей калорийностью не более {calories_remained} кКал'
+            return self.LEFT.format(calories_remained)
         else:
-            return f'Хватит есть!'
+            return self.STOP
 
 
 class CashCalculator(Calculator):
 
-    EURO_RATE = 91.92
-    USD_RATE = 78.1
+    EURO_RATE = 70.0
+    USD_RATE = 60.0
 
-    def __init__(self, limit):
-        super().__init__(limit)
+    d_currency = {
+        'eur': ['Euro', EURO_RATE],
+        'usd': ['USD', USD_RATE],
+        'rub': ['руб', 1]}
+
+    LEFT = 'На сегодня осталось {} {}'
+    NO = 'Денег нет, держись'
+    DEBT = 'Денег нет, держись: твой долг - {} {}'
 
     def get_today_cash_remained(self, currency):
 
         today_stats = self.get_today_stats()
-
-        if currency == 'eur':
-            cash_remained = (self.limit - today_stats)/self.EURO_RATE
-            cur = 'Euro'
-        elif currency == 'rub':
-            cash_remained = self.limit - today_stats
-            cur = 'руб'
-        elif currency == 'usd':
-            cash_remained = (self.limit - today_stats)/self.USD_RATE
-            cur = 'USD'
-        else:
-            cash_remained = self.limit - today_stats
-            cur = 'руб'
+        cash_remained = (self.limit - today_stats)/self.d_currency[currency][1]
+        cur = self.d_currency[currency][0]
 
         if cash_remained > 0:
-            return f'На сегодня осталось {round(cash_remained, 2)} {cur}'
+            return self.LEFT.format(round(cash_remained, 2), cur)
         elif cash_remained == 0:
-            return 'Денег нет, держись'
+            return self.NO
         else:
-            return f'Денег нет, держись: твой долг - ' \
-                f'{round(abs(cash_remained), 2)} {cur}'
-
+            return self.DEBT.format(round(abs(cash_remained), 2), cur)
