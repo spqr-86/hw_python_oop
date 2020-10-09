2707f4b62301206649ce0af0a9c870fd202be2ae
Petr <spqr86.sr@gmail.com>
2020-10-09 15:51:58 +0800

    4

--- homework.py
+++ homework.py
@@ -2,23 +2,23 @@ import datetime as dt
 
 
 class Calculator:
+
     def __init__(self, limit: float):
         self.limit = limit
         self.records = []
 
-    today = dt.date.today()
-
     def add_record(self, record):
-
         self.records.append(record)
 
     def get_today_stats(self):
-        return sum(e.amount for e in self.records if e.date == self.today)
+        today = dt.date.today()
+        return sum(e.amount for e in self.records if e.date == today)
 
     def get_week_stats(self):
-        return sum(e.amount for e in self.records if
-                   (self.today + dt.timedelta(1)) > e.date >=
-                   (self.today - dt.timedelta(7)))
+        today = dt.date.today()
+        weak = today - dt.timedelta(7)
+
+        return sum(e.amount for e in self.records if weak < e.date <= today)
 
 
 class Record:
@@ -37,46 +37,40 @@ class Record:
 
 class CaloriesCalculator(Calculator):
 
-    LEFT = 'Сегодня можно съесть что-нибудь ещё, ' \
-           'но с общей калорийностью не более {} кКал'
-
+    LEFT = ('Сегодня можно съесть что-нибудь ещё, '
+            'но с общей калорийностью не более {} кКал')
     STOP = 'Хватит есть!'
 
     def get_calories_remained(self):
-
         today_stats = self.get_today_stats()
-
         calories_remained = self.limit - today_stats
-
         if calories_remained > 0:
             return self.LEFT.format(calories_remained)
-        else:
-            return self.STOP
+        return self.STOP
 
 
 class CashCalculator(Calculator):
 
     EURO_RATE = 70.0
     USD_RATE = 60.0
-
-    d_currency = {
+    currency_items = {
         'eur': ['Euro', EURO_RATE],
         'usd': ['USD', USD_RATE],
         'rub': ['руб', 1]}
-
-    LEFT = 'На сегодня осталось {} {}'
+    LEFT = 'На сегодня осталось {cash} {currency}'
     NO = 'Денег нет, держись'
-    DEBT = 'Денег нет, держись: твой долг - {} {}'
+    DEBT = 'Денег нет, держись: твой долг - {cash} {currency}'
 
     def get_today_cash_remained(self, currency):
-
         today_stats = self.get_today_stats()
-        cash_remained = (self.limit - today_stats)/self.d_currency[currency][1]
-        cur = self.d_currency[currency][0]
-
-        if cash_remained > 0:
-            return self.LEFT.format(round(cash_remained, 2), cur)
-        elif cash_remained == 0:
+        limit = self.limit - today_stats
+        if limit == 0:
             return self.NO
-        else:
-            return self.DEBT.format(round(abs(cash_remained), 2), cur)
+        rate = self.currency_items[currency][1]
+        signature = self.currency_items[currency][0]
+        cash = round(limit/rate, 2)
+        if limit > 0:
+            return self.LEFT.format(cash=cash,
+                                    currency=signature)
+        return self.DEBT.format(cash=abs(cash),
+                                currency=signature)
